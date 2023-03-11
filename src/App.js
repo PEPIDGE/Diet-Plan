dadc233da85e25724f7db16db95728f7f97d25eb
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-11 19:37:37 +0200

    Making a home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import { Header } from './components/Header';
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
-   <Header/>
+   <>
+    <Header/>
+    <HomePage/>
+   </>
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+    return(
+        <div className="home">
+            <h1>HELLO WORLD!</h1>
+        </div>
+    );
+}
