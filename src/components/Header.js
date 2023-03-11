c1cbabd68042d9ab23eae7a6fa0b839c327888df
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-11 16:54:54 +0200

    Header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,29 @@
 import { Link as a } from "react-router-dom";
 
 export const Header = () => {
-
-  const logged = 
-    (<div id="user">
-      <a to="/create">Create Game</a>
-      <a to="/logout">Logout</a>
-    </div>)
-  const guest = 
-    (
-      <div id="guest">
-          <a to="/login">Login</a>
-          <a to="/register">Register</a>
-      </div>
-    )
+//   const logged = (
+//     <div id="user">
+//       <a to="/create">Create Game</a>
+//       <a to="/logout">Logout</a>
+//     </div>
+//   );
+//   const guest = (
+//     <div id="guest">
+//       <a to="/login">Login</a>
+//       <a to="/register">Register</a>
+//     </div>
+//   );
   return (
-    <header>
-      <nav>
-        <a to=""></a>
-        <a to="/catalog">All games</a>
-        {/*authData.accessToken ? logged : guest*/}
-        
-      </nav>
-    </header>
-    
+    <nav>
+    <div className="logo">
+      <a href="#"><img src="./images/logo.png" alt="Your Logo"/></a>
+    </div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
   );
 };
