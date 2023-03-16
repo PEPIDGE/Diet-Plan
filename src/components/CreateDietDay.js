cde7c93c98a2d8e897c950e43fd7f97f7f0ad380
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-16 22:09:10 +0200

    Updated the create diet day form

--- src/components/CreateDietDay.js
+++ src/components/CreateDietDay.js
@@ -6,7 +6,8 @@ export const CreateDietDay = () => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        
+        const data = Object.fromEntries(new FormData(event.target))
+        console.log(data);
     }
 
     return(
@@ -14,19 +15,46 @@ export const CreateDietDay = () => {
             <h1>Your diet day</h1>
             <form onSubmit={submitHandler} className="create-form">
             
-            <label htmlFor="email">Breakfast</label>
-            <textarea type="text" id="breakfast" name="breakfast" placeholder="Enter your breakfast" />
+            <div className="create-form-div">
+                <div>
+                    <label htmlFor="email">Breakfast</label>
+                    <input type="text" id="breakfast" name="breakfast" placeholder="Enter your breakfast" />
+                </div>
+                <div>
+                    <label htmlFor="email">Grams</label>
+                    <input type="number" id="breakfast-weight" name="breakfast-weight" placeholder="Weight of the breakfast" />
+                </div>
 
-            <label htmlFor="lunch">Lunch</label>
-            <textarea type="text" id="lunch" name="lunch" placeholder="Enter your lunch" />
+                <div>
+                    <label htmlFor="lunch">Lunch</label>
+                    <input type="text" id="lunch" name="lunch" placeholder="Enter your lunch" />
+                </div>
+                <div>
+                    <label htmlFor="lunch">Grams</label>
+                    <input type="number" id="lunch-weight" name="lunch-weight" placeholder="Weight of the lunch" />
+                </div>
 
-            <label htmlFor="afternoon">Afternoon snack</label>
-            <textarea type="text" id="afternoon" name="afternoon" placeholder="Enter your afternoon snack" />
+                <div>
+                    <label htmlFor="afternoon">Afternoon snack</label>
+                    <input type="text" id="afternoon" name="afternoon" placeholder="Enter your afternoon snack" />
+                </div>
+                <div>
+                    <label htmlFor="afternoon">Grams</label>
+                    <input type="number" id="afternoon-weight" name="afternoon-weight" placeholder="Weight of the afternoon snack" />
+                </div>
 
-            <label htmlFor="dinner">Dinner</label>
-            <textarea type="text" id="dinner" name="dinner" placeholder="Enter your dinner" />
+                <div>
+                    <label htmlFor="dinner">Dinner</label>
+                    <input type="text" id="dinner" name="dinner" placeholder="Enter your dinner" />
+                </div>
+                <div>
+                    <label htmlFor="dinner">Grams</label>
+                    <input type="number" id="dinner-weight" name="dinner-weight" placeholder="Weight of the dinner" />
+                </div>
 
-            <input className="btn submit-form-btn" type="submit" value="Submit"/>
+            </div>
+
+            <input className="btn create-form-btn" type="submit" value="Submit"/>
             </form>
         </div>
     );
