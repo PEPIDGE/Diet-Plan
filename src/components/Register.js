d4334586fefe2faf80a522b287d8564ded84fc5f
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-17 15:43:56 +0200

    updated register page
    its a controlled form and updated logic

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,33 +2,45 @@ import { useState } from "react";
 
 export const Register = () => {
 
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [rePassword, setRePassword] = useState('');
+    const [data, setData] = useState({
+        "username": "",
+        "email": "",
+        "description": "",
+        "profilePic": "",
+        "password": "",
+        "rePassword": ""
+    });
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        console.log(`Userame: ${username} \n Email: ${email} \n Password: ${password}`);
+    function inputHandler (event) {
+        const { name, value } = event.target;
+        setData({ ...data, [name]: value });
     }
 
+   console.log(data);
     return(
         <div className="form-container">
             <h1>REGISTER</h1>
-            <form onSubmit={submitHandler} className="register-form">
+            <form className="register-form">
 
             <label htmlFor="name">Username</label>
-            <input type="text" id="username" name="username" value={username} onChange={(event) => setUsername(event.target.value)} placeholder="Enter your username" />
+            <input type="text" id="username" name="username" value={data.username} onChange={inputHandler} placeholder="Enter your username" />
 
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter your email" />
+            <input type="email" id="email" name="email" value={data.email} onChange={inputHandler} placeholder="Enter your email" />
+
+            <label htmlFor="password">Profile picture</label>
+            <input type="url" id="profilePic" name="profilePic" value={data.profilePic} onChange={inputHandler} placeholder="Post a profile picture link" />
+
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter your password" />
+            <input type="password" id="password" name="password" value={data.password} onChange={inputHandler} placeholder="Enter your password" />
 
-            <label htmlFor="password">Re-Password</label>
-            <input type="password" id="rePassword" name="rePassword" value={rePassword} onChange={(event) => setRePassword(event.target.value)} placeholder="Re-enter your password" />
+            <label htmlFor="rePassword">Re-Password</label>
+            <input type="password" id="rePassword" name="rePassword" value={data.rePassword} onChange={inputHandler} placeholder="Re-enter your password" />
 
+            <label htmlFor="password">Write how you want to be described to others</label>
+            <textarea maxLength="100" type="text" id="description" name="description" value={data.description} onChange={inputHandler} placeholder="Enter description about you" />
+            
             <input className="btn submit-form-btn" type="submit" value="Submit"/>
             </form>
         </div>
