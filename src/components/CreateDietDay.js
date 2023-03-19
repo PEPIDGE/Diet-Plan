77302d9668a867b6a7f43c668b5f2c66ea460bbe
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-19 17:23:21 +0200

    Updated create diet day business logic
    Instead of grams it is calories

--- src/components/CreateDietDay.js
+++ src/components/CreateDietDay.js
@@ -20,8 +20,8 @@ export const CreateDietDay = () => {
                     <input type="text" id="breakfast" name="breakfast" placeholder="Enter your breakfast" />
                 </div>
                 <div>
-                    <label htmlFor="email">Grams</label>
-                    <input type="number" id="breakfast-weight" name="breakfast-weight" placeholder="Weight of the breakfast" />
+                    <label htmlFor="email">Calories</label>
+                    <input type="number" id="breakfast-weight" name="breakfast-weight" placeholder="Kilocalories of the breakfast" />
                 </div>
 
                 <div>
@@ -29,8 +29,8 @@ export const CreateDietDay = () => {
                     <input type="text" id="lunch" name="lunch" placeholder="Enter your lunch" />
                 </div>
                 <div>
-                    <label htmlFor="lunch">Grams</label>
-                    <input type="number" id="lunch-weight" name="lunch-weight" placeholder="Weight of the lunch" />
+                    <label htmlFor="lunch">Calories</label>
+                    <input type="number" id="lunch-weight" name="lunch-weight" placeholder="Kilocalories of the lunch" />
                 </div>
 
                 <div>
@@ -38,8 +38,8 @@ export const CreateDietDay = () => {
                     <input type="text" id="afternoon" name="afternoon" placeholder="Enter your afternoon snack" />
                 </div>
                 <div>
-                    <label htmlFor="afternoon">Grams</label>
-                    <input type="number" id="afternoon-weight" name="afternoon-weight" placeholder="Weight of the afternoon snack" />
+                    <label htmlFor="afternoon">Calories</label>
+                    <input type="number" id="afternoon-weight" name="afternoon-weight" placeholder="Kilocalories of the afternoon snack" />
                 </div>
 
                 <div>
@@ -47,8 +47,8 @@ export const CreateDietDay = () => {
                     <input type="text" id="dinner" name="dinner" placeholder="Enter your dinner" />
                 </div>
                 <div>
-                    <label htmlFor="dinner">Grams</label>
-                    <input type="number" id="dinner-weight" name="dinner-weight" placeholder="Weight of the dinner" />
+                    <label htmlFor="dinner">Calories</label>
+                    <input type="number" id="dinner-weight" name="dinner-weight" placeholder="Kilocalories of the dinner" />
                 </div>
 
             </div>
