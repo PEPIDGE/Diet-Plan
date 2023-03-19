6ae1e2559dec1867c0c5cf4e01f4e58c64ac0457
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-19 17:21:38 +0200

    Register form validation update
    validate if the fields are empty, and the description max is 350, when you reach 300 it shows a countdown from the 50 symbols you have remaining

--- src/components/Register.js
+++ src/components/Register.js
@@ -19,6 +19,20 @@ export const Register = () => {
         "rePassword": ""
     });
 
+    let symbolsRemaing = 0;
+    useEffect(() => {
+        let errors = {...dataErrors}
+        if (data.description.length >=300 && data.description.length < 350) {
+            symbolsRemaing = 350 - data.description.length;
+            errors.description =`You have ${symbolsRemaing} symbols remaingn`;
+        } else if (data.description.length <300) {
+            errors.description ="";
+        } else if (data.description.length === 350) {
+            errors.description =`You've reached your maximum symbols`;
+        }
+        setDataErrors(errors); 
+    }, [data.description])
+
     function inputHandler (event) {
         const { name, value } = event.target;
         setData({ ...data, [name]: value });
@@ -39,21 +53,45 @@ export const Register = () => {
             "rePassword": ""
         };
         let isValid = true;
+        
         if (data.username.trim().length <2 || data.username.trim().length > 8) {
             isValid = false;
             errors.username = "The length of the username must be between 2 and 8 symbols" ;
         }
         if (data.profilePic.length > 0 && data.profilePic.trim().slice(-4) !== ".jpg" && data.profilePic.trim().slice(-4) !== ".png") {
             isValid = false;
-            errors.profilePic = "The type of photo format must be .png or .jpg"  ;
+            errors.profilePic = "The type of photo format must be .png or .jpg";
         }
         if (data.password.trim().length <10 || data.password.trim().length > 30) {
             isValid = false;
-            errors.password = "The length of the username must be between 10 and 30 symbols" ;
+            errors.password = "The length of the password must be between 10 and 30 symbols";
         }
         if (data.password !== data.rePassword) {
             isValid = false;
             errors.rePassword = "The passwords don't match" ;
+        }
+        if (data.description.length > 0 && data.description.trim().length < 15) {
+            isValid = false;
+            errors.description = "The length of the description must be at least 15 symbols";
+        } else if (data.description.trim().length > 300) {
+            isValid = false;
+            errors.description = "The length of the description must be maximum 300 symbols";
+        }
+        if (data.username.trim().length === 0 ) {
+            isValid = false;
+            errors.username = "The field is required" ;
+        }
+        if (data.email.trim().length === 0 ) {
+            isValid = false;
+            errors.email = "The field is required" ;
+        }
+        if (data.password.trim().length === 0 ) {
+            isValid = false;
+            errors.password = "The field is required" ;
+        }
+        if (data.rePassword.trim().length === 0 ) {
+            isValid = false;
+            errors.rePassword = "The field is required" ;
         }
         setDataErrors(errors)
         return isValid;
@@ -63,35 +101,35 @@ export const Register = () => {
             <h1>REGISTER</h1>
             <form className="register-form" onSubmit={submitHandler}>
 
-            <label htmlFor="name">Username</label>
+            <label htmlFor="name">Username <span className="required">*</span></label>
             <input type="text" id="username" name="username" value={data.username} onChange={inputHandler} placeholder="Enter your username" />
             {dataErrors.username.length > 0 ? <div className="error">{dataErrors.username}</div> : ""}
 
             
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email <span className="required">*</span></label>
             <input type="email" id="email" name="email" value={data.email} onChange={inputHandler} placeholder="Enter your email" />
 
             {dataErrors.email.length > 0 ? <div className="error">{dataErrors.email}</div> : ""}
 
 
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password <span className="required">*</span></label>
             <input type="password" id="password" name="password" value={data.password} onChange={inputHandler} placeholder="Enter your password" />
 
             {dataErrors.password.length > 0 ? <div className="error">{dataErrors.password}</div> : ""}
 
 
-            <label htmlFor="rePassword">Re-Password</label>
+            <label htmlFor="rePassword">Re-Password <span className="required">*</span></label>
             <input type="password" id="rePassword" name="rePassword" value={data.rePassword} onChange={inputHandler} placeholder="Re-enter your password" />
 
             {dataErrors.rePassword.length > 0 ? <div className="error">{dataErrors.rePassword}</div> : ""}
 
-            <label htmlFor="profilePic">Profile picture <span>(optional)</span></label>
+            <label htmlFor="profilePic">Profile picture <span className="optional">(optional)</span></label>
             <input type="text" id="profilePic" name="profilePic" value={data.profilePic} onChange={inputHandler} placeholder="Enter a profile picture link" />
 
             {dataErrors.profilePic.length > 0 ? <div className="error">{dataErrors.profilePic}</div> : ""}
 
-            <label htmlFor="description">Write how you want to be described to others <span>(optional)</span></label>
-            <textarea maxLength="100" type="text" id="description" name="description" value={data.description} onChange={inputHandler} placeholder="Enter description about you" />
+            <label htmlFor="description">Write how you want to be described to others <span className="optional">(optional)</span></label>
+            <textarea maxLength={350} type="text" id="description" name="description" value={data.description} onChange={inputHandler} placeholder="Enter description about you" />
             
             {dataErrors.description.length > 0 ? <div className="error">{dataErrors.description}</div> : ""}
 
