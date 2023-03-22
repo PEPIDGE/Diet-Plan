bd20d6f30b3800535344a7410a2b81f7a6bba96a
PEPIDGE <pepkatabrat123@gmail.com>
2023-03-22 23:40:13 +0200

    added button for updating the profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,22 +1,30 @@
+import { Link } from "react-router-dom";
+
 export const MyProfile = () => {
+
+  const userId = JSON.parse(localStorage.getItem("authData"))._id;
+  
+
     return (
         <>
-  <h1 className="title">My Profile</h1>
-  <div className="container">
-    <div className="profile">
-      <img src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="Profile picture" />
-      <h1>Username</h1>
-      <h3>Descripton:</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum
-        quis lectus sed interdum. Nunc eget aliquam est. Integer semper
-        tincidunt massa, sed efficitur nisi rhoncus at. Donec tincidunt, sem vel
-        consequat lobortis, elit elit consectetur purus, in imperdiet lacus
-        risus at ex.
-      </p>
-    </div>
-  </div>
-</>
+          <h1 className="title">My Profile</h1>
+          <div className="container">
+            <div className="profile">
+              <img src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg" alt="Profile picture" />
+              <Link to={`/updateProfile/${userId}`} className="btn update-profile-btn">Update profile</Link>
+              
+              <h1>Username</h1>
+              <h3>Descripton:</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis bibendum
+                quis lectus sed interdum. Nunc eget aliquam est. Integer semper
+                tincidunt massa, sed efficitur nisi rhoncus at. Donec tincidunt, sem vel
+                consequat lobortis, elit elit consectetur purus, in imperdiet lacus
+                risus at ex.
+              </p>
+            </div>
+          </div>
+        </>
 
     );
 }
